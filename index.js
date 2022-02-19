0bfb65c8be593645bf897af292e1fd4779b9ecc8
Victor Wang <hongjenwang@gmail.com>
2022-02-19 12:43:43 -0800

    create counter app

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let countEl = document.getElementById("count-el");
+
+let saveEl = document.getElementById("save-el");
+
+let count = 0
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+};
+function decrease() {
+    count -= 1;
+    countEl.textContent = count;
+};
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.textContent = count
+};
+
+// function clearFunction() {
+//     countEl.innerText = 0
+//     saveEl.innerText = ""
+// };
+
